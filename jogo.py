30bf2da6c39d20d2d66185e94ff11ce54dff245b
KayqueMBM <kayquemarcelom@gmail.com>
2025-04-10 11:23:45 -0300

    jogo em python

---
+++ jogo.py
@@ -0,0 +1,69 @@
+import pygame
+
+pygame.init()
+
+#Dimensões da tela
+x = 1280
+y = 720
+
+screen = pygame.display.set_mode((x, y))
+pygame.display.set_caption("Meu jogo em Python")
+
+cenario = pygame.image.load('imagens/espaço.jpg').convert_alpha()
+cenario = pygame.transform.scale(cenario, (x, y))
+
+alien = pygame.image.load('imagens/alien.png').convert_alpha()
+alien = pygame.transform.scale(alien, (50, 50))
+
+nave = pygame.image.load('imagens/nave.png').convert_alpha()
+nave = pygame.transform.scale(nave, (50, 50)) # Conversão de tamanho da nave
+nave = pygame.transform.rotate(nave, -90)
+
+pos_alien_x = 500
+pos_alien_y = 300
+
+pos_nave_x = 200
+pos_nave_y = 300
+
+
+# Variável para movimento vertical
+scroll_y = 0
+
+# Velocidade vertical do alien
+vel_alien_y = -1
+
+rodando = True
+
+while rodando:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            rodando = False
+
+    # Coordenada relativa no eixo y
+    rel_y = scroll_y % cenario.get_rect().height
+    screen.blit(cenario, (0, rel_y - cenario.get_rect().height)) # Desenha a primeira parte do cenario
+    if rel_y < y:
+        screen.blit(cenario, (0, rel_y)) # Desenha a segunda parte do cenario
+
+    # Teclas
+    tecla = pygame.key.get_pressed()
+    if tecla[pygame.K_UP] and pos_nave_y > 1:
+        pos_nave_y -= 2
+    if tecla[pygame.K_DOWN] and pos_nave_y < 665:
+        pos_nave_y += 2
+    
+    # Movimento do cenario no eixo vertical
+    scroll_y += 1
+
+    # Movimento vertical do alien
+    pos_alien_y += vel_alien_y
+
+    # Se atingir os limites da tela, inverte a direção
+    if pos_alien_y <= 0 or pos_alien_y >= y -50:
+        vel_alien_y *= -1
+
+    # Criar imagens
+    screen.blit(alien, (pos_alien_x, pos_alien_y))
+    screen.blit(nave, (pos_nave_x, pos_nave_y))
+    
+    pygame.display.flip()
